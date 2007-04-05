920b9ebc064142deffcb08f1195a382ea8057e16
Pat Thoyts <patthoyts@users.sourceforge.net>
2007-04-05 22:57:52 +0000

    Fix vfs pkgIndex for tclkits which link this in statically
    git-svn-id: svn://svn.equi4.com/kitgen/trunk@1405 9e558909-932a-0410-a563-af77432da1eb

--- files/vfs1.3-pkgIndex.tcl
+++ files/vfs1.3-pkgIndex.tcl
@@ -1,25 +1,7 @@
-namespace eval ::vfs {}
-variable vfs::dll [file join $dir libvfs1.3.dylib]
-
-proc loadvfs {dll} {
-    global auto_path
-    if {![file exists $dll]} { return }
-    set dir [file dirname $dll]
-    if {[lsearch -exact $auto_path $dir] == -1} {
-    	lappend auto_path $dir
-    }
-    load $dll
-}
-
-package ifneeded vfs        1.3 [list loadvfs $vfs::dll]
-
-# Allow optional redirect of VFS_LIBRARY components.  Only necessary
-# for testing, but could be used elsewhere.
-if {[info exists ::env(VFS_LIBRARY)]} { set dir $::env(VFS_LIBRARY) }
+package ifneeded vfs        1.3   [list load {} vfs]
 package ifneeded starkit    1.3.1 [list source [file join $dir starkit.tcl]]
 package ifneeded vfslib     1.3.1 [list source [file join $dir vfslib.tcl]]
 package ifneeded vfs::mk4    1.10 [list source [file join $dir mk4vfs.tcl]]
 package ifneeded vfs::zip     1.0 [list source [file join $dir zipvfs.tcl]]
-
 #compat
 package ifneeded mk4vfs      1.10 [list source [file join $dir mk4vfs.tcl]]
