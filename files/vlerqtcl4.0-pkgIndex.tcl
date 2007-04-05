924083145745373d6f61f1258d2a1d147b25d57d
Jean-Claude Wippler <jcw@equi4.com>
2007-04-05 23:10:15 +0000

    forgot to rename vlerqtcl to vqtcl
    git-svn-id: svn://svn.equi4.com/kitgen/trunk@1406 9e558909-932a-0410-a563-af77432da1eb

--- files/vlerqtcl4.0-pkgIndex.tcl
+++
@@ -1,5 +0,0 @@
-package ifneeded vlerq 4.0 {load "" vlerq}
-
-package ifneeded mklite    0.4 [list source [file join $dir mklite.tcl]]
-package ifneeded vfs::m2m  1.7 [list source [file join $dir m2mvfs.tcl]]
-package ifneeded vfs::mkcl 1.5 [list source [file join $dir mkclvfs.tcl]]
