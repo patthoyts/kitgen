fc9ec15027108ede96166ca4c39eac5677b25a74
Pat Thoyts <patthoyts@users.sourceforge.net>
2007-12-24 00:07:15 +0000

    Fixed the version number (this really should be automatic)
    git-svn-id: svn://svn.equi4.com/kitgen/trunk@1981 9e558909-932a-0410-a563-af77432da1eb

--- files/tk8.5-pkgIndex.tcl
+++ files/tk8.5-pkgIndex.tcl
@@ -1,4 +1,4 @@
-package ifneeded Tk 8.5b3 \
+package ifneeded Tk 8.5.0 \
   [string map [list @@ [file join $dir .. libtk8.5[info sharedlibext]]] {
     if {[lsearch -exact [info loaded] {{} Tk}] >= 0} {
       load "" Tk
